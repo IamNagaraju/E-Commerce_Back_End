a4ebc59d5600aa6e24371c97d9808c2ed26488f0
Nagaraju <nagaraju@Nagarajus-MacBook-Air.local>
2019-02-07 15:07:10 +0530

    modifications

--- app/controllers/categories_controller.js
+++ app/controllers/categories_controller.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const _= require('lodash');
+const _ = require('lodash');
 
 const router = express.Router();
 
@@ -8,15 +8,15 @@ const { authorizeUser } = require('../middlewares/authentication');
 const { Category } = require('../models/category');
 
 // To get all the categories
-router.get('/',(req,res) => {
-  Category.find().then( categories => {
+router.get('/', (req, res) => {
+  Category.find().then(categories => {
     res.send(categories);
   }).catch(err => {
     res.send(err)
   })
 })
 
-router.get('/:id', (req,res) => {
+router.get('/:id', (req, res) => {
   let id = req.params.id;
   Category.findById(id).then((categories) => {
     res.send(categories);
@@ -26,11 +26,11 @@ router.get('/:id', (req,res) => {
 })
 
 //To post a category
-router.post('/', authenticateUser , authorizeUser, (req,res) => {
-  let body =_.pick(req.body,['name']);
+router.post('/', authenticateUser, authorizeUser, (req, res) => {
+  let body = _.pick(req.body, ['name']);
   let category = new Category(body);
   console.log(category)
-  category.save().then( category => {
+  category.save().then(category => {
     // console.log(category)
     res.send(category)
   }).catch(err => {
@@ -39,27 +39,31 @@ router.post('/', authenticateUser , authorizeUser, (req,res) => {
 })
 
 // To update for one category
-router.put('/:id', authenticateUser,authorizeUser,(req,res) => {
+router.put('/:id', authenticateUser, authorizeUser, (req, res) => {
   let id = req.params.id;
-  let body =_.pick(req.body,['name']);
+  let body = _.pick(req.body, ['name']);
 
-  Category.findByIdAndUpdate(id, {$set : body }, {new : true}).then(category => {
-    res.send({category,
-       notice:'successfully updated'})
+  Category.findByIdAndUpdate(id, { $set: body }, { new: true }).then(category => {
+    res.send({
+      category,
+      notice: 'successfully updated'
+    })
   }).catch(err => {
-    res.send({notice:'id not found'})
+    res.send({ notice: 'id not found' })
   })
 })
 
 // To delete one category
-router.delete('/:id',authenticateUser,authorizeUser,(req,res) => {
+router.delete('/:id', authenticateUser, authorizeUser, (req, res) => {
   let id = req.params.id;
-  Category.findOneAndRemove({_id:id}).then((deletedData) => {
-    res.send({deletedData,
-    notice:'successfully delted'});
+  Category.findOneAndRemove({ _id: id }).then((deletedData) => {
+    res.send({
+      deletedData,
+      notice: 'successfully delted'
+    });
   }).catch(err => {
     res.send(err)
   })
 })
 
-module.exports = { categoriesController : router }
+module.exports = { categoriesController: router }
